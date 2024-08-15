a58eeabfa94a872ddb4aca77b2fb3542e56ac6c6
Ivan Martins <ivanmartins.ilha@gmail.com>
2024-08-15 00:18:01 -0300

    fix: adjust url api

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,7 +1,7 @@
 const API_KEY = 'ECA1AB4CE8583613A2C759B445E98';
 
 export async function cadastrarUsuario(userData) {
-  const response = await fetch('/api/cadastro', {
+  const response = await fetch('http://localhost:8080', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
